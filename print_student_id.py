99b25172e3002cf22f8241ace6ecd526d01e68c7
nearyourheart <124992629+nearyourheart@users.noreply.github.com>
2023-02-10 14:59:58 +0700

    yahooo

--- print_student_id.py
+++ print_student_id.py
@@ -1,6 +1,11 @@
+import numpy as numpy
 
 def print_id(student_id):
     print("Student ID = ", student_id)
 
+def calculate_sum(array):
+    return array.sum()
+
 if __name__ == "__main__":
     print_id("6513370")
+    calculate_sum(np.array([1,2,3]))
